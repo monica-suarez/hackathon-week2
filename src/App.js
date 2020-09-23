8fa6d95bb88adede8306f46821f09fcf2f933d90
monica-suarez <shutterbugmo@gmail.com>
2020-09-23 11:49:09 -0500

    added api url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      stories: []
+    }
+  }
+  componentDidMount(){
+    fetch('https://hn.algolia.com/api/v1/search_by_date?tags=&hitsPerPage=25')
+    .then(response => response.json())
+    .then(data => this.setState({
+      stories: data
+    }
+    ))
+  }
+  render(){
   return (
     <div className="App">
-    <h1>Welcome to our 411 Hackathon!</h1>
+    <h1 className="title">Welcome to our 411 Hackathon!</h1>
     </div>
   );
+  }
 }
 
 export default App;
