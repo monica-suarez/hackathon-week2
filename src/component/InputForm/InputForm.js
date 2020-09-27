b9f919481c2586e9aacf5493fa3a65e19cd80f04
monica-suarez <shutterbugmo@gmail.com>
2020-09-26 20:45:48 -0500

    getting form up

--- src/component/InputForm/InputForm.js
+++ src/component/InputForm/InputForm.js
@@ -1,1 +1,30 @@
-// import React from 'react'; 
+import React from 'react'; 
+
+
+
+
+const InputForm = (author, title, date) =>{
+ return(
+    <form>
+    <select>
+        <option className="placeholder" value="" defaultValue hidden>Select Search Criteria</option>
+        <option value='author' name='author' author={author}>Author</option>
+        <option value='title' name='title' title={title}>Title</option>
+        <option value='date' name='date' date={date}>Date</option>
+    </select>
+    <br/>
+    <br/>
+    <input type="date"/>
+    <br/>
+    <br/>
+    <input type="type" /> 
+    <br/>
+    <br/>
+    <button>Submit</button>
+    <br/>
+    <br/>
+    </form>
+ )
+}
+
+export default InputForm;
